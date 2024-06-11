b3f0c90e9c645359e595fbeaa7c87ff13786c74b
Matthias Prinke <matthias.prinke@zeiss.com>
2024-06-11 12:32:36 +0200

    Added from PythonApiExamples

---
+++ AppExamples/projects/ExampleProjects/doc/README.md
@@ -0,0 +1,7 @@
+## ExampleProjects
+
+This App is part of the [ZEISS INSPECT Python API Examples](https://zeissiqs.github.io/zeiss-inspect-addon-api/2025/python_examples/index.html).
+
+It provides ZEISS INSPECT projects for trying the Python API examples.
+
+You can [download this App](https://software-store.zeiss.com/products/apps/ExampleProjects) from the [ZEISS Quality Software Store](https://software-store.zeiss.com).
---
+++ AppExamples/projects/ExampleProjects/scripts/ExampleProjects/setup_project.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# setup_project.py
+#
+# Carl Zeiss GOM Metrology GmbH, 2024
+#
+# This App is part of the ZEISS INSPECT Python API Examples:
+# https://zeissiqs.github.io/zeiss-inspect-addon-api/2025/python_examples/
+# ---
+import gom
+
+
+def open_project(project_name, force_reopen=False):
+	# if project already open, do nothing (except force_reopen)
+	if (hasattr(gom.app, 'project') and gom.app.project.project_name == project_name and not force_reopen):
+		return
+
+	# Close current project (discard changes)
+	gom.script.sys.close_project()
+
+	tmp_dir = gom.app.temp_directory
+	tmp_dir_project = tmp_dir + '/' + project_name + '.zinspect'
+
+	# Write example project to tmp dir
+	project_resource = gom.Resource(':ExampleProjects/projects/' + project_name + '.zinspect')
+	with project_resource.open() as src, open(tmp_dir_project, 'wb') as target:
+		target.write(src.read())
+
+	# Open example project
+	gom.script.sys.load_project(file=tmp_dir_project)
+
+
+if __name__ == '__main__':
+
+	DIALOG = gom.script.sys.create_user_defined_dialog(file='setup_project.gdlg')
+	DIALOG.projectlist.items = ['zeiss_part_test_project', 'volume_test_project', 'zeiss_part_test_measurement']
+
+	RESULT = gom.script.sys.show_user_defined_dialog(dialog=DIALOG)
+	print("Opening project:", RESULT.projectlist)
+	open_project(RESULT.projectlist, True)
