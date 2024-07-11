4bae10ed3b790e32e4c42fd4b185cafcae5d8291
Matthias Prinke <matthias.prinke@zeiss.com>
2024-07-11 13:37:41 +0200

    Added layout variant for sphinx_doc, added category descriptions

--- .github/scripts/gen_overview.py
+++ .github/scripts/gen_overview.py
@@ -1,8 +1,39 @@
+#
+# gen_overview.py
+#
+# Carl Zeiss GOM Metrology GmbH, 2024
+#
+# ---
+'''Generate ZEISS INSPECT App Examples Overview markdown file'''
+
 import os
 import json
+import argparse
 
+#
+# Parse command line arguments
+#
+parser = argparse.ArgumentParser()
+
+# autopep8: off
+parser.add_argument('--sphinx-doc', action='store_true', help='Select sphinx_doc layout')
+# autopep8: on
+
+args = parser.parse_args()
+sphinx_doc = args.sphinx_doc
 
 BASEDIR = 'AppExamples'
+
+CATEGORY_DESCRIPTIONS = {
+    'data_interfaces': 'How to access data of ZEISS INSPECT elements',
+    'dialog_widgets': 'How to use custom dialogs and handle user input events',
+    'misc': 'Miscellaneous',
+    'script_icons': 'How to set icons for scripts or buttons',
+    'script_resources': 'How to access binary data of your App (resources)',
+    'scripted_actuals': 'Building custom actual elements with Python code',
+    'scripted_checks': 'Building custom checks with Python code',
+    'projects': 'ZEISS INSPECT projects'
+}
 
 EXAMPLE_PROJECTS = {
     'zeiss_part_test_project': {'index': 1},
@@ -10,71 +41,209 @@ EXAMPLE_PROJECTS = {
     'volume_test_project': {'index': 3}
 }
 
-APP_OVERVIEW = "# ZEISS INSPECT App Examples Overview\n"
+APP_OVERVIEW = ""
 
-TABLE_HEADER  = "| App | Description | Example Projects | References |\n"
-TABLE_HEADER += "| --- | ----------- | ---------------- | ---------- |\n"
-#APP_OVERVIEW  = "| Category | App | Description | Example Projects | References |\n"
-#APP_OVERVIEW += "| -------- | --- | ----------- | ---------------- | ---------- |\n"
+if sphinx_doc:
+    APP_OVERVIEW += \
+"""---
+myst:
+   html_meta:
+      "description": "Examples for using the ZEISS INSPECT 2023 App Python API"
+      "keywords": "Metrology, ZEISS INSPECT, Python API, GOM API, Scripting, Add-ons, Apps, Examples"
+---
+"""
+
+APP_OVERVIEW += "# ZEISS INSPECT App Examples Overview\n"
+
+
+tagIndex = {}
+
+def gen_table(category):
+    """
+    Generate App overview as table
+
+    | App | Description | Example Projects | References | Tags |
+    | --- | ----------- | ---------------- | ---------- | ---- |
+    """
+
+    # App table header
+    # Using non-breaking spaces (&nbsp;) to enforce a common/minimum column width
+    md  = f"| {24*'&nbsp;'}App{24*'&nbsp;'} | {36*'&nbsp;'}Description{36*'&nbsp;'} | Example Projects | References | {7*'&nbsp;'}Tags{7*'&nbsp;'} |\n"
+    md +=  "| --- | ----------- | ---------------- | ---------- | ---- |\n"
+
+    apps = os.path.join(BASEDIR, category)
+    for app in os.listdir(apps):
+        metainfo_path = os.path.abspath(os.path.join(BASEDIR, category, app, 'metainfo.json'))
+        with open(metainfo_path, 'r', encoding="utf-8") as f:
+            metainfo = json.load(f)
+            if sphinx_doc:
+                view = f"https://github.com/ZEISS/zeiss-inspect-app-examples/blob/dev/AppExamples/{category}/{app}/doc/Documentation.md"
+            else:
+                view = f"{category}/{app}/doc/Documentation.md"
+            download = f"https://software-store.zeiss.com/products/apps/{metainfo['title']}"
+
+            # Title, links to source code and App download
+            title = metainfo['title']
+            md += f"| <a id=\"{title}\">{title}</a><br>[view]({view}) / [download]({download}) | "
+
+            # Description
+            md += f"{metainfo['description'].encode('windows-1252').decode('utf-8')} |"
+
+            # Example projects
+            if 'example-projects' in metainfo:
+                for example in metainfo['example-projects']:
+                    if example in EXAMPLE_PROJECTS:
+                        md += f" [{EXAMPLE_PROJECTS[example]['index']})](#example-projects) "
+            md += " | "
+
+            # References
+            if 'references' in metainfo:
+                for reference in metainfo['references']:
+                    md += f"[{reference[0]}]({reference[1]})<br>"
+
+            md += " | "
+
+            # Tags
+            for tag in metainfo['tags']:
+                if not tag in tagIndex:
+                    tagIndex[tag] = []
+                tagIndex[tag].append(title)
+                badge = tag.replace('-', '--')
+                if sphinx_doc:
+                    md += f"<a href=\"#{tag}\">![Static Badge](https://img.shields.io/badge/{badge}-blue)</a><br>"
+                else:
+                    md += f"[![Static Badge](https://img.shields.io/badge/{badge}-blue)](#{tag})<br>"
+            md += " |"
+
+            md += "\n"
+            return md
+
+def gen_boilerplate(category):
+    """
+        Generate App overview as table
+
+        ### App 
+        
+        Description
+            ...
+
+        Example Projects
+            ...
+        
+        References
+            ...
+        
+        Tags
+            ...
+    """
+
+    md = ""
+
+    apps = os.path.join(BASEDIR, category)
+    for app in os.listdir(apps):
+        metainfo_path = os.path.abspath(os.path.join(BASEDIR, category, app, 'metainfo.json'))
+        with open(metainfo_path, 'r', encoding="utf-8") as f:
+            metainfo = json.load(f)
+            if sphinx_doc:
+                view = f"https://github.com/ZEISS/zeiss-inspect-app-examples/blob/dev/AppExamples/{category}/{app}/doc/Documentation.md"
+            else:
+                view = f"{category}/{app}/doc/Documentation.md"
+            download = f"https://software-store.zeiss.com/products/apps/{metainfo['title']}"
+
+            # Title, links to source code and App download
+            title = metainfo['title']
+            md += f"### <a id=\"{title}\">{title}</a> &mdash; [view]({view}) / [download]({download})\n\n"
+
+            #md += f"![Icon](https://github.com/ZEISS/zeiss-inspect-app-examples/blob/dev/AppExamples/{category}/{app}/icon.png)\n"
+
+            # Description
+            md += ":Description:\n"
+            md += f"    {metainfo['description'].encode('windows-1252').decode('utf-8')}\n\n"
+
+            # Example projects
+            if 'example-projects' in metainfo:
+                md += ":Example Projects:\n"
+                for example in metainfo['example-projects']:
+                    if example in EXAMPLE_PROJECTS:
+                        md += f"    [{example}](#example-projects)\n"
+                md += "\n"
+            
+            # References
+            if 'references' in metainfo:
+                md += ":References:\n"
+                for reference in metainfo['references']:
+                    md += f"    [{reference[0]}]({reference[1]})\n"
+
+                md += "\n"
+
+            # Tags
+            if len(metainfo['tags']):
+                md += ":Tags:\n"
+                md += "    "
+                for tag in metainfo['tags']:
+                    if not tag in tagIndex:
+                        tagIndex[tag] = []
+                    tagIndex[tag].append(title)
+                    badge = tag.replace('-', '--')
+                    if sphinx_doc:
+                        md += f"<a href=\"#{tag}\">![Static Badge](https://img.shields.io/badge/{badge}-blue)</a> "
+                    else:
+                        md += f"[![Static Badge](https://img.shields.io/badge/{badge}-blue)](#{tag})<br> "
+
+                md += "\n"
+    return md
 
 categories = os.listdir(BASEDIR)
 for category in categories:
     if category == '.gitignore':
         continue
-    #print(' Category:', category)
-    #print(os.path.abspath(category))
-
-    # Category heading
-    APP_OVERVIEW += f"\n## {category}\n\n"
-
-    APP_OVERVIEW += TABLE_HEADER
 
     if os.path.isdir(os.path.abspath(os.path.join(BASEDIR, category))):
-        apps = os.path.join(BASEDIR, category)
-        #print(apps)
-        for app in os.listdir(apps):
-            #print("  App:", app)
-            metainfoPath = os.path.abspath(os.path.join(BASEDIR, category, app, 'metainfo.json'))
-            with open(metainfoPath, 'r', encoding="utf-8") as f:
-                metainfo = json.load(f)
-                #print(metainfo)
-                #print(metainfo['description'])
-                view = f"{category}/{app}/doc/Documentation.md"
-                download = f"https://software-store.zeiss.com/products/apps/{metainfo['title']}"
 
-                # Category & Title, links to source code and App download 
-                APP_OVERVIEW += f"| {metainfo['title']}<br>[view]({view}) / [download]({download}) | "
+        # Category heading
+        APP_OVERVIEW += f"\n## {category} &mdash; {CATEGORY_DESCRIPTIONS[category]}\n\n"
 
-                # Description
-                APP_OVERVIEW += f"{metainfo['description'].encode('windows-1252').decode('utf-8')} |"
+        if sphinx_doc:
+            APP_OVERVIEW += gen_boilerplate(category)
+        else:
+            APP_OVERVIEW += gen_table(category)
 
-                # Example projects
-                if 'example-projects' in metainfo:
-                    for example in metainfo['example-projects']:
-                        if example in EXAMPLE_PROJECTS:
-                            APP_OVERVIEW += f" [{EXAMPLE_PROJECTS[example]['index']})](#example-projects) "
-                APP_OVERVIEW += " | "
-                
-                # References
-                if 'references' in metainfo:
-                    for reference in metainfo['references']:
-                        APP_OVERVIEW += f"[{reference[0]}]({reference[1]})<br>"
-
-                APP_OVERVIEW += " |"
-
-                APP_OVERVIEW += "\n"
 
 # Example projects
 APP_OVERVIEW += "\n## Example projects\n\n"
 
-for example in EXAMPLE_PROJECTS:
-    APP_OVERVIEW += f"{EXAMPLE_PROJECTS[example]['index']}) {example}\n"
+for example, infos in EXAMPLE_PROJECTS.items():
+    if sphinx_doc:
+        APP_OVERVIEW += f"* {example}\n"
+    else:
+        APP_OVERVIEW += f"{infos['index']}) {example}\n"
 
 APP_OVERVIEW += "\n[Download Example Projects App](https://software-store.zeiss.com/products/apps/ExampleProjects)"
+
+
+# Tag index
+APP_OVERVIEW += "\n\n## Tag Index\n"
+
+for tag in sorted(tagIndex):
+    #APP_OVERVIEW += f"\n### {tag}:\n"
+    badge = tag.replace('-', '--')
+    if sphinx_doc:
+        APP_OVERVIEW += f"\n### <a id=\"{tag}\">![Static Badge](https://img.shields.io/badge/{badge}-blue)<a>\n\n"
+    else:
+        APP_OVERVIEW += f"\n### ![Static Badge](https://img.shields.io/badge/{badge}-blue)\n\n"
+
+    for app in sorted(tagIndex[tag]):
+        if sphinx_doc:
+            APP_OVERVIEW += f"* <a href=\"#{app}\">{app}</a>\n"
+        else:
+            APP_OVERVIEW += f"* [{app}](#{app})\n"
+    APP_OVERVIEW += "\n"
+
 
 # Related links 
 APP_OVERVIEW += "\n## Related\n\n"
 APP_OVERVIEW += "* [ZEISS IQS GitHub &mdash; App Development Documentation](https://zeissiqs.github.io/zeiss-inspect-addon-api/2025/index.html)\n"
 APP_OVERVIEW += "* [ZEISS Quality Software Store](https://software-store.zeiss.com)\n"
+
 
 print(APP_OVERVIEW)
