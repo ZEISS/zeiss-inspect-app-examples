16514b1598972b96730483c98912415f37af1f49
Matthias Prinke <matthias.prinke@zeiss.com>
2025-07-23 11:11:51 +0200

    Restuctured

--- AppExamples/misc/PytestTemplate/scripts/uut_project_keywords.py
+++ AppExamples/misc/PytestTemplate/scripts/uut_project_keywords.py
@@ -15,11 +15,11 @@ import gom
 # Note: This entire script is run by test_blackbox
 ####################################################################################
 
-# Create a new project. This will raise an exception if a project is already open - which is fine.
-try:
-    gom.script.sys.create_project ()
-except:
-    pass
+
+PROJECT_KEYWORDS = {
+    'project': {'description': 'Project Name', 'value': 'Test Project'},
+    'inspector': {'description': 'Inspector', 'value': 'Clouseau'}
+}
 
 def not_covered_by_test():
     '''This is not covered by any test'''
@@ -44,18 +44,48 @@ def get_project_keywords():
     print(keywords)
     return keywords
 
-PROJECT_KEYWORDS = {
-    'project': {'description': 'Project Name', 'value': 'Test Project'},
-    'inspector': {'description': 'Inspector', 'value': 'Clouseau'}
-}
+def edit_project_keywords():
+    '''Edit project keywords in user-defined dialog'''
+    DIALOG=gom.script.sys.create_user_defined_dialog (file='dialog.gdlg')
 
-if __name__ == "__main__":
+    #
+    # Event handler function called if anything happens inside of the dialog
+    #
+    def dialog_event_handler (widget):
+        if widget == 'initialize':
+            DIALOG.project.value = gom.app.project.get('user_project')
+            DIALOG.inspector.value = gom.app.project.get('user_inspector')
+
+    DIALOG.handler = dialog_event_handler
+
+    RESULT=gom.script.sys.show_user_defined_dialog (dialog=DIALOG)
+    gom.script.sys.set_project_keywords(keywords={'project': RESULT.project})
+    gom.script.sys.set_project_keywords(keywords={'inspector': RESULT.inspector})
+
+    print("\n-- Edited project keywords --")
+    for k in gom.app.project.project_keywords:
+        print(f"{k}='{gom.app.project.get(k)}'")
+
+    return RESULT
+
+def main():
+    """Main"""
     for k, v in PROJECT_KEYWORDS.items():
         gom.script.sys.set_project_keywords(
             keywords={k:v['value']},
             keywords_description={k:v['description']}
         )
 
-    print("-- Project keywords --")
+    print("-- Initial project keywords --")
     for k in gom.app.project.project_keywords:
         print(f"{k}='{gom.app.project.get(k)}'")
+
+if __name__ == "__main__":
+    # Create a new project.
+    # This will raise an exception if a project is already open - which is fine.
+    try:
+        gom.script.sys.create_project ()
+    except gom.RequestError:
+        pass
+
+    main()
