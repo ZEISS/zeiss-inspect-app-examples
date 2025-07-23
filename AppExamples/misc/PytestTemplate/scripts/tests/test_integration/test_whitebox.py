9050113a4c5c5a4aff35f89385644d8757f789a4
Matthias Prinke <matthias.prinke@zeiss.com>
2025-07-23 11:04:29 +0200

    Modified

--- AppExamples/misc/PytestTemplate/scripts/tests/test_integration/test_whitebox.py
+++ AppExamples/misc/PytestTemplate/scripts/tests/test_integration/test_whitebox.py
@@ -15,9 +15,13 @@ import uut_project_keywords
 
 
 def test_whitebox():
+    gom.script.sys.close_project()
+    gom.script.sys.create_project()
+    
     '''Executing a UUT function'''
     expected_keywords = {'user_inspector': {'description': 'Inspector', 'value': 'Clouseau'},
                          'user_project': {'description': 'Project Name', 'value': 'Test Project'}}
 
+    uut_project_keywords.main()
     actual = uut_project_keywords.get_project_keywords()
     assert actual == expected_keywords
