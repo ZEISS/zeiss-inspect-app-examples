0105d94048fb2ff83a340e5d64a89120db6cfdb0
Matthias Prinke <matthias.prinke@zeiss.com>
2025-08-22 11:10:08 +0200

    Added test for service

---
+++ AppExamples/misc/PytestTemplate/scripts/tests/test_integration/test_service.py
@@ -0,0 +1,29 @@
+""" Integration test example for service
+
+NOTE:
+The test runner's pytest code coverage cannot include any services,
+because they run in separate Python interpreter processes.
+
+Instead, a dedicated coverage setup is used for services -
+see services/reflector.py
+
+Carl Zeiss GOM Metrology GmbH, 2025
+"""
+
+import gom
+import time
+import gom.api.services
+from app_utils.service_manager import ServiceManager
+	
+def test_service():
+	""" Test a service
+	
+	The ServiceManager context manager starts and stops the service,
+	which is especially useful if the App is not installed, but used
+	from a connected folder. 
+  """
+	
+	with ServiceManager("Pytest Reflector"):
+		from gom.api.myservice import reflect
+	
+		assert reflect({"answer": 42}) == {"answer": 42}
