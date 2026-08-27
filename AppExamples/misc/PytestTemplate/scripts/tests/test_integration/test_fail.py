8f3c3deefc968da16925d3d953037890d08e5fbd
Matthias Prinke <matthias.prinke@zeiss.com>
2026-08-27 16:01:16 +0200

    Mark test as intentionally failing

--- AppExamples/misc/PytestTemplate/scripts/tests/test_integration/test_fail.py
+++ AppExamples/misc/PytestTemplate/scripts/tests/test_integration/test_fail.py
@@ -1,10 +1,12 @@
 """ Dummy test which always fails
 
-Carl Zeiss GOM Metrology GmbH, 2025
+Carl Zeiss GOM Metrology GmbH, 2026
 """
 
 import gom
+import pytest
 
+@pytest.mark.xfail(reason="Intentional failure test")
 def test_fail():
     """This test has no unit-under-test and always fails"""
     assert False
